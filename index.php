16182c4171793d827e7133133019cc0961debb28
agtomi <agtomi@gmail.com>
2018-07-10 21:35:37 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,104 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+    <head>
+        <title>Eleven Ház Fitness és Fallabda Órarend</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="description" content="Eleven Ház Fitness és Fallabda Órarend" />
+        <link rel="stylesheet" href="css/orarend.css" type="text/css" media="screen"/>
+    </head>
+
+    <body>
+       <div id="content">
+            <a class="back" href=""></a>
+                <span class="scroll"></span>
+                <div class="head"> 
+                    <div id="logo"></div>
+                    <div id="headszoveg">SZAUNABEOSZTÁS<br>2018 Március </div>
+                </div>
+            
+            <table class="table1">
+                <thead>
+                    <tr>
+<!--                        <th></th>-->
+                        <th scope="col" abbr="Szauna">  </th>
+                        <th scope="col" abbr="Hétfő">Hétfő</th>
+                        <th scope="col" abbr="Kedd">Kedd</th>
+                        <th scope="col" abbr="Szerda">Szerda</th>
+                        <th scope="col" abbr="Csütörtök">Csütörtök</th>
+                        <th scope="col" abbr="Péntek">Péntek</th>
+                        <th scope="col" abbr="Szombat">Szombat</th>
+                        <th scope="col" abbr="Szombat">Vasárnap</th>
+                    </tr>
+                </thead>
+               
+                <tbody>
+            </tr>
+           </tr>
+         </tr>
+        <td><br><br><br><br><b>finn<br><br>gőz</b></td>
+        <td><b>Február 26. 06.00-21.00</b><br><br>jégmenta<br> <br>Levendula</td>
+        <td><b>Február 27. 06.00-21.00</b><br><br>eukaliptusz<br> <br>Levendula</td> 
+        <td class="kiemeltpiros"><b>Február 28. 06.00-21.00</b><br>FINN NAP<br>Erdeifenyő<br>+fenyőkátrány<br>Nyírfalevél</td> 
+         <td><b>Március 01. 06.00-21.00</b><br><br> jázmin<br> <br>levendula</td>
+        <td class="kiemeltcitrom"><b>.Március 02. 06.00-21.00</b><br>FÜVESNAP<br>CITROM<br> <br>CITROMFŰ</td>
+         <td class="kiemeltsarga"><b>Március 03. 08.00-20.00</b><br><br>eukaliptusz<br> <br>levendula</td>
+        <td class="kiemeltpiros"><b>Március 04. 10.00-18.00</b><br>MEDITERRÁN NAP<br>jázmin<br>levendula</td>
+      
+  
+        <tr>
+        <td><br><br><br><br><b>finn<br><br>gőz</b></td>
+        <td><b>Március 05. 06.00-21.00</b><br><br>eukaliptusz<br> <br>levendula</td>
+        <td><b>Március 06. 06.00-21.00</b><br><br>jégmenta<br> <br>Levendula</td> 
+        <td class="kiemeltzöld"><b>Március 07. 06.00-21.00</b><br><br> BORSMENTA<br> <br>levendula</td> 
+        <td><b>Március 08. 06.00-21.00.00</b><br><br>eukaliptusz<br> <br>Levendula</td>
+        <td class="kiemeltcitrom"><b>Március 09. 06.00-21.00</b><br>FÜVESNAP<br>CITROM<br><br>CITROMFŰ</td>
+        <td class="kiemeltsarga"><b>Március 10. 08.00-20.00</b><br>MEDITERRÁN NAP<br>Töröknarancs<br>levendula</td>
+        <td class="kiemeltpiros"><b>Március 11. 10.00-18.00</b><br>FINN NAP<br>Erdeifenyő<br>+fenyőkátrány<br>Nyírfalevél</td>
+          </tr>
+     <tr>     <tr>
+        <td><br><br><br><br><b>finn<br><br>gőz</b></td>
+        <td><b>Március 12. 06.00-21.00</b><br><br>eukaliptusz<br> <br>levendula</td>
+        <td class="kiemeltpiros"><b>Március 13. 06.00-21.00</b><br>FINN NAP<br>Erdeifenyő<br>+fenyőkátrány<br>Nyírfalevél</td> 
+        <td class="kiemeltsarga"><b>Március 14. 06.00-21.00</b><br>MEDITERRÁN NAP<br>Töröknarancs<br>levendula</td>
+        <td><b>Március 15. 10.00-18.00</b><br><br>borsmenta<br> <br>Kakukkfű</td>
+        <td><b>Március 16. 08.00-20.00</b><br><br>jázmin<br> <br>levendula</td> 
+        <td class="kiemeltcitrom"><b>Március 17. 08.00-20.00</b><br>FÜVESNAP<br>CITROM<br> <br>CITROMFŰ</td>
+        <td><b>Március 18. 10.00-18.00</b><br><br>eukaliptusz<br> <br>Levendula</b></td> 
+     
+     <tr>
+        <td><br><br><br><br><b>finn<br><br>gőz</b></td>
+        <td><b>Március 19. 06.00-21.00</b><br><br>eukaliptusz<br> <br>Levendula</td>
+        <td><b>Március 20. 06.00-21.00</b><br><br>jégmenta<br> <br>Levendula</td> 
+        <td class="kiemeltzöld"><b>Március 21. 06.00-21.00</b><br><br> BORSMENTA<br> <br>levendula</td>
+        <td><b>Március 22. 06.00-21.00</b><br><br>eukaliptusz<br> <br>kakukkfű</td>
+        <td class="kiemeltcitrom"><b>Március 23. 06.00-21.00</b><br>FÜVESNAP<br>CITROM-MENTA<br> <br>CITROMFŰ</td>
+        <td class="kiemeltsarga"><b>Március 24. 08.00-20.00</b><br>MEDITERRÁN NAP<br>Töröknarancs<br>levendula</td>
+        <td class="kiemeltpiros"><b>Március 25. 10.00-18.00</b><br>FINN NAP<br>Erdeifenyő<br>+fenyőkátrány<br>Nyírfalevél</td>
+          </tr>
+ <tr>
+         </tr>
+         </tr>
+        <td><br><br><br><br><b>finn<br><br>gőz</b></td>
+        <td><b>Március 26. 06.00-21.00</b><br><br>jégmenta<br> <br>Levendula</td>
+        <td><b>Március 27. 06.00-21.00</b><br><br>eukaliptusz<br> <br>Levendula</td> 
+        <td class="kiemeltpiros"><b>Március 28. 06.00-21.00</b><br>FINN NAP<br>Erdeifenyő<br>+fenyőkátrány<br>Nyírfalevél</td> 
+         <td><b>Március 29. 06.00-21.00</b><br><br> jázmin<br> <br>levendula</td>
+        <td class="kiemeltcitrom"><b>.Március 30. 06.00-21.00</b><br>FÜVESNAP<br>CITROM<br> <br>CITROMFŰ</td>
+         <td class="kiemeltsarga"><b>Március 31. 08.00-20.00</b><br><br>eukaliptusz<br> <br>levendula</td>
+        <td class="kiemeltpiros"><b>Április 01. 10.00-18.00</b><br>MEDITERRÁN NAP<br>jázmin<br>levendula</td>
+      
+        <td></td>
+        <td></td>
+        <tr>
+      </tbody>
+        </table>  
+           <div class="szoveg">
+               <p><b>Finn szaunánk 90 C-os, 8 - 10 fő számára, míg a gőzszaunánk 50 C-os, automata illatadagolással 4 fő számára közösségi szaunák. Infra szaunánk kíméletes 35-40 C-on működik, a lábzónában igény szerint lekapcsolható a fűtés. Az infra szauna bejelentkezéssel foglalható max 2 fő számára. Bejelentkezés az Infraszaunára  :</b></p>
+               <p>Tel.:+36(57) 430-551 </p>
+               <p>Mobil: +36(30) 53-57-257</p>
+                  
+            </div>
+        </div>
+
+    </body>
+</html>
