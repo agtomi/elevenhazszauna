ee699bed85b8e7d5f0d1822e2a3f33aa7aa921d0
agtomi <agtomi@gmail.com>
2018-11-27 21:33:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
     <head>
-        <title>Eleven Ház Fitness és Fallabda Órarend</title>
+        <title>Eleven Ház Fitness és Fallabda Szauna</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <meta name="description" content="Eleven Ház Fitness és Fallabda Órarend" />
         <link rel="stylesheet" href="css/orarend.css" type="text/css" media="screen"/>
