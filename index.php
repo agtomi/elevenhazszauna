bc76fa2b0cbe2ab27dee7cccfff68e9900960b0b
agtomi <agtomi@gmail.com>
2018-11-27 21:50:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
     <head>
-        <title>Eleven Ház Fitness és Fallabda Órarend</title>
+        <title>Eleven Ház Fitness és Fallabda Szauna</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-        <meta name="description" content="Eleven Ház Fitness és Fallabda Órarend" />
+        <meta name="description" content="Eleven Ház Fitness és Fallabda Szauna" />
         <link rel="stylesheet" href="css/orarend.css" type="text/css" media="screen"/>
         <style>
             img { 
@@ -12,8 +12,7 @@
         </style>
     </head>
 
-<body>
-    <div id="content">                
+<body>              
     <div id="headszoveg">Aktuális hét szaunaprogramja</div>
     <div><img src="https://www.dropbox.com/s/erwlyzwqxzojfya/heti%20szauna%201.jpg?dl=1" alt="Aktuális hét szaunaprogramja"></div>   
     <div id="headszoveg">Jövő hét szaunaprogramja</div>    
@@ -22,7 +21,6 @@
       <p><b>Finn szaunánk 90 C-os, 8 - 10 fő számára, míg a gőzszaunánk 50 C-os, automata illatadagolással 4 fő számára közösségi szaunák. Infra szaunánk kíméletes 35-40 C-on működik, a lábzónában igény szerint lekapcsolható a fűtés. Az infra szauna bejelentkezéssel foglalható max 2 fő számára. Bejelentkezés az Infraszaunára:</b></p>
       <p>Tel.:+36(57) 430-551 </p>
       <p>Mobil: +36(30) 53-57-257</p>           
-    </div>
     </div>
 </body>
 </html>
