bc354d465a9d18a704c8a5e32e561fc807b34322
agtomi <agtomi@gmail.com>
2018-11-27 21:32:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
     <head>
         <title>Eleven Ház Fitness és Fallabda Órarend</title>
@@ -7,98 +7,17 @@
         <link rel="stylesheet" href="css/orarend.css" type="text/css" media="screen"/>
     </head>
 
-    <body>
-       <div id="content">
-            <a class="back" href=""></a>
-                <span class="scroll"></span>
-                <div class="head"> 
-                    <div id="logo"></div>
-                    <div id="headszoveg">SZAUNABEOSZTÁS<br>2018 November </div>
-                </div>
-            
-            <table class="table1">
-                <thead>
-                    <tr>
-<!--                        <th></th>-->
-                        <th scope="col" abbr="Szauna">  </th>
-                        <th scope="col" abbr="Hétfő">Hétfő</th>
-                        <th scope="col" abbr="Kedd">Kedd</th>
-                        <th scope="col" abbr="Szerda">Szerda</th>
-                        <th scope="col" abbr="Csütörtök">Csütörtök</th>
-                        <th scope="col" abbr="Péntek">Péntek</th>
-                        <th scope="col" abbr="Szombat">Szombat</th>
-                        <th scope="col" abbr="Szombat">Vasárnap</th>
-                    </tr>
-                </thead>
-               
-                <tbody>
-            </tr>
-           </tr>
-         </tr>
-        <td><br><br><br><br><b>finn<br><br>gőz</b></td>
-        <td><b></td>
-        <td><b></td> 
-        <td><b></td> 
-         <td><b>November 01. SZÜNNAP</b></td>
-        <td class="kiemeltcitrom"><b>November 02. 06.00-21.00</b><br>FÜVESNAP<br>CITROM<br> <br>CITROMFŰ</td>
-         <td class="kiemeltsarga"><b>November 03. 08.00-20.00</b><br><br>eukaliptusz<br> <br>levendula</td>
-        <td class="kiemeltpiros"><b>November 04. 10.00-18.00</b><br>MEDITERRÁN NAP<br>jázmin<br>levendula</td>
-      
-  
-        <tr>
-        <td><br><br><br><br><b>finn<br><br>gőz</b></td>
-        <td><b>November 05. 06.00-21.00</b><br><br>eukaliptusz<br> <br>levendula</td>
-        <td><b>November 06. 06.00-21.00</b><br><br>jégmenta<br> <br>Levendula</td> 
-        <td class="kiemeltzöld"><b>November 07. 06.00-21.00</b><br><br> BORSMENTA<br> <br>levendula</td> 
-        <td><b>November 08. 06.00-21.00.00</b><br><br>eukaliptusz<br> <br>Levendula</td>
-        <td class="kiemeltcitrom"><b>November 09. 06.00-21.00</b><br>FÜVESNAP<br>CITROM<br><br>CITROMFŰ</td>
-        <td class="kiemeltsarga"><b>November 10. 08.00-20.00</b><br>MEDITERRÁN NAP<br>Töröknarancs<br>levendula</td>
-        <td class="kiemeltpiros"><b>November 11. 10.00-18.00</b><br>FINN NAP<br>Erdeifenyő<br>+fenyőkátrány<br>Nyírfalevél</td>
-          </tr>
-     <tr>     <tr>
-        <td><br><br><br><br><b>finn<br><br>gőz</b></td>
-        <td><b>November 12. 06.00-21.00</b><br><br>eukaliptusz<br> <br>levendula</td>
-        <td class="kiemeltpiros"><b>November 13. 06.00-21.00</b><br>FINN NAP<br>Erdeifenyő<br>+fenyőkátrány<br>Nyírfalevél</td> 
-        <td class="kiemeltsarga"><b>November 14. 06.00-21.00</b><br>MEDITERRÁN NAP<br>Töröknarancs<br>levendula</td>
-        <td><b>November 15. 06.00-21.00</b><br><br>borsmenta<br> <br>Kakukkfű</td>
-        <td><b>November 16. 06.00-21.00</b><br><br>jázmin<br> <br>levendula</td> 
-        <td class="kiemeltcitrom"><b>November 17. 08.00-20.00</b><br>FÜVESNAP<br>CITROM<br> <br>CITROMFŰ</td>
-        <td><b>November 18. 10.00-18.00</b><br><br>eukaliptusz<br> <br>Levendula</b></td> 
-     
-     <tr>
-        <td><br><br><br><br><b>finn<br><br>gőz</b></td>
-        <td><b>November 19. 06.00-21.00</b><br><br>eukaliptusz<br> <br>Levendula</td>
-        <td><b>November 20. 06.00-21.00</b><br><br>jégmenta<br> <br>Levendula</td> 
-        <td class="kiemeltzöld"><b>November 21. 06.00-21.00</b><br><br> BORSMENTA<br> <br>levendula</td>
-        <td><b>November 22. 06.00-21.00</b><br><br>eukaliptusz<br> <br>kakukkfű</td>
-        <td class="kiemeltcitrom"><b>November23. 06.00-21.00</b><br>FÜVESNAP<br>CITROM-MENTA<br> <br>CITROMFŰ</td>
-        <td class="kiemeltsarga"><b>November 24. 08.00-20.00</b><br>MEDITERRÁN NAP<br>Töröknarancs<br>levendula</td>
-        <td class="kiemeltpiros"><b>November 25. 10.00-18.00</b><br>FINN NAP<br>Erdeifenyő<br>+fenyőkátrány<br>Nyírfalevél</td>
-          </tr>
- <tr>
-         </tr>
-         </tr>
-        <td><br><br><br><br><b>finn<br><br>gőz</b></td>
-        <td><b>November 26. 06.00-21.00</b><br><br>jégmenta<br> <br>Levendula</td>
-        <td><b>November 27. 06.00-21.00</b><br><br>eukaliptusz<br> <br>Levendula</td> 
-        <td class="kiemeltpiros"><b>November 28. 06.00-21.00</b><br>FINN NAP<br>Erdeifenyő<br>+fenyőkátrány<br>Nyírfalevél</td> 
-         <td><b>November 29. 06.00-21.00</b><br><br> jázmin<br> <br>levendula</td>
-        <td class="kiemeltcitrom"><b>.November 30. 06.00-21.00</b><br>FÜVESNAP<br>CITROM<br> <br>CITROMFŰ</td>
-         <td class="kiemeltsarga"><b>December 01. 08.00-20.00</b><br><br>eukaliptusz<br> <br>levendula</td>
-        <td class="kiemeltpiros"><b>December 02. 10.00-18.00</b><br>MEDITERRÁN NAP<br>jázmin<br>levendula</td>
-      
-        <td></td>
-        <td></td>
-        <tr>
-      </tbody>
-        </table>  
-           <div class="szoveg">
-               <p><b>Finn szaunánk 90 C-os, 8 - 10 fő számára, míg a gőzszaunánk 50 C-os, automata illatadagolással 4 fő számára közösségi szaunák. Infra szaunánk kíméletes 35-40 C-on működik, a lábzónában igény szerint lekapcsolható a fűtés. Az infra szauna bejelentkezéssel foglalható max 2 fő számára. Bejelentkezés az Infraszaunára  :</b></p>
-               <p>Tel.:+36(57) 430-551 </p>
-               <p>Mobil: +36(30) 53-57-257</p>
-                  
-            </div>
-        </div>
-    </body>
-
+<body>
+   <div id="content">                
+   <div id="headszoveg">Aktuális hét szaunaprogramja</div>
+   <img src="https://www.dropbox.com/s/erwlyzwqxzojfya/heti%20szauna%201.jpg?dl=1" alt="Aktuális hét szaunaprogramja" >
+   <div id="headszoveg">Jövő hét szaunaprogramja</div>    
+   <img src="https://www.dropbox.com/s/m0jylb89hmb1i90/heti%20szauna%202.jpg?dl=1" alt="Jövő hét szaunaprogramja" >
+   <div class="szoveg">
+      <p><b>Finn szaunánk 90 C-os, 8 - 10 fő számára, míg a gőzszaunánk 50 C-os, automata illatadagolással 4 fő számára közösségi szaunák. Infra szaunánk kíméletes 35-40 C-on működik, a lábzónában igény szerint lekapcsolható a fűtés. Az infra szauna bejelentkezéssel foglalható max 2 fő számára. Bejelentkezés az Infraszaunára:</b></p>
+      <p>Tel.:+36(57) 430-551 </p>
+      <p>Mobil: +36(30) 53-57-257</p>           
+    </div>
+    </div>
+</body>
 </html>
