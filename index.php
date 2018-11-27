046db4d30f64b94a0e1771e44428eb5b13d6ba12
agtomi <agtomi@gmail.com>
2018-11-27 22:23:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,15 +12,17 @@
         </style>
     </head>
 
-<body scroll="no"> 
-    <div id="headszoveg">Aktuális hét szaunaprogramja</div>
-    <div><img src="https://www.dropbox.com/s/erwlyzwqxzojfya/heti%20szauna%201.jpg?dl=1" alt="Aktuális hét szaunaprogramja"></div>   
-    <div id="headszoveg">Jövő hét szaunaprogramja</div>    
-    <div><img src="https://www.dropbox.com/s/m0jylb89hmb1i90/heti%20szauna%202.jpg?dl=1" alt="Jövő hét szaunaprogramja"></div>
-   <div class="szoveg">
-      <p><b>Finn szaunánk 90 C-os, 8 - 10 fő számára, míg a gőzszaunánk 50 C-os, automata illatadagolással 4 fő számára közösségi szaunák. Infra szaunánk kíméletes 35-40 C-on működik, a lábzónában igény szerint lekapcsolható a fűtés. Az infra szauna bejelentkezéssel foglalható max 2 fő számára. Bejelentkezés az Infraszaunára:</b></p>
-      <p>Tel.:+36(57) 430-551 </p>
-      <p>Mobil: +36(30) 53-57-257</p>           
+<body>
+    <div id="content">
+        <div id="headszoveg">Aktuális hét szaunaprogramja</div>
+        <div><img src="https://www.dropbox.com/s/erwlyzwqxzojfya/heti%20szauna%201.jpg?dl=1" alt="Aktuális hét szaunaprogramja"></div>   
+        <div id="headszoveg">Jövő hét szaunaprogramja</div>    
+        <div><img src="https://www.dropbox.com/s/m0jylb89hmb1i90/heti%20szauna%202.jpg?dl=1" alt="Jövő hét szaunaprogramja"></div>
+       <div class="szoveg">
+          <p><b>Finn szaunánk 90 C-os, 8 - 10 fő számára, míg a gőzszaunánk 50 C-os, automata illatadagolással 4 fő számára közösségi szaunák. Infra szaunánk kíméletes 35-40 C-on működik, a lábzónában igény szerint lekapcsolható a fűtés. Az infra szauna bejelentkezéssel foglalható max 2 fő számára. Bejelentkezés az Infraszaunára:</b></p>
+          <p>Tel.:+36(57) 430-551 </p>
+          <p>Mobil: +36(30) 53-57-257</p>           
+        </div>
     </div>
 </body>
 </html>
